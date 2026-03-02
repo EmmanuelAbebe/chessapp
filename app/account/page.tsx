209b44d35b7ea671f85c6c48ac8b77ca85ae4d06
mray <emmanuelmihret@gmail.com>
2026-03-02 15:36:01 -0500

    update:initial account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const page = () => {
-  return <div>account infomation</div>;
+  return (
+    <div>
+      <div>
+        <div>UserName</div>
+        <div>Chess level</div>
+        <div>stats</div>
+        <div>settings</div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
